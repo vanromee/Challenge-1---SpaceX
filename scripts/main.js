c7e3bd32de7020d8502df95d1d24dd24c5cf5797
vanromee <58208601+vanromee@users.noreply.github.com>
2020-05-10 18:49:32 +0200

    intikaise

---
+++ scripts/main.js
@@ -0,0 +1,100 @@
+
+
+// PARAMETERS
+var shipsParameters = {
+							Fuel: '200',
+							Throttle: 'Intact',
+							Speed: '17,500 miles/h',
+							currentSpeed: 100,
+};
+
+function accelerate(){
+							shipsParameters.currentSpeed += 10;
+							console.log(shipsParameters.currentSpeed);
+							document.getElementById("speed").innerHTML = shipsParameters.currentSpeed;
+							}
+
+
+	document.getElementById("fuel").innerHTML = shipsParameters.Fuel;
+	document.getElementById("throttle").innerHTML = shipsParameters.Throttle;
+	document.getElementById("speed").innerHTML = shipsParameters.currentSpeed;
+
+
+
+// ENVIRONMENT
+
+ var shipsEnvironment = {
+							Gravity: '300',
+							Atmosphere: '',
+							currentDistance: 0,
+
+							distance: function(){ 	
+							console.log ('km');
+							this.currentDistance += 500;
+						}
+};
+
+
+// Environment
+	document.getElementById("gravity").innerHTML = shipsEnvironment.Gravity;
+	document.getElementById("atmosphere").innerHTML = shipsEnvironment.Atmosphere;
+	document.getElementById("distance-travelled").innerHTML = shipsEnvironment.currentDistance;
+
+
+// console.log(shipsEnvironment.currentDistance);
+
+// shipsEnvironment.distance();
+
+// console.log(shipsEnvironment.currentDistance);
+
+
+
+//SUPPLIES
+
+var supplies = {
+							Food: '',
+							Water: ''
+};
+
+
+
+//METRICS
+
+function convert1(){
+	
+	 var x = document.getElementById("myNumber1").value;
+	 var newX = x * 4;
+  document.getElementById("convert1").innerHTML = newX;
+}
+
+function convert2(){
+	
+	 var x = document.getElementById("myNumber2").value;
+	 var newX = x * 3;
+  document.getElementById("convert2").innerHTML = newX;
+}
+
+document.getElementById("convert1").innerHTML = "Result";
+document.getElementById("convert2").innerHTML = "Result";
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Supplies
+	document.getElementById("water").innerHTML = '70%';
+	document.getElementById("food").innerHTML = '96%';
+
+
+
+
+
+
